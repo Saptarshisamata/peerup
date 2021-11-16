4625b3c1701a0a3348e1ee14115d6919b52441f9
saptarshi <saptarshisamanta97@gmail.com>
2021-11-16 20:25:02 +0530

    connection

--- src/routes/connection.route.js
+++ src/routes/connection.route.js
@@ -8,5 +8,7 @@ const router = express.Router()
 // use jwt_auth in production 
 router.get('/suggested_people',connectionController.suggestedPeoples)
 router.post('/send_req',jwt_auth,connectionController.sendRequest)
-router.get('/request_received_list',connectionController.connectionRequestReceivedList)
+router.get('/request_received_list',jwt_auth,connectionController.connectionRequestReceivedList)
+router.get('/request_send_list',jwt_auth,connectionController.connectionRequestSendList)
+
 module.exports = router
